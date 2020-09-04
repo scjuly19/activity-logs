183ef53f42b2a250f2f9e51c09ec4df7808e1568
Sakshi <scjuly69@gmail.com>
2020-09-05 00:55:08 +0530

    Updates

---
+++ precache-manifest.8c87af89f9b5565e9d4f644b4f18df4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195f302a5afb59809a94921c93a79afc",
+    "url": "/activity-logs/index.html"
+  },
+  {
+    "revision": "5ca87bce91b304042154",
+    "url": "/activity-logs/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "20829c8a2b3d664a149f",
+    "url": "/activity-logs/static/css/main.8ef6bb6c.chunk.css"
+  },
+  {
+    "revision": "5ca87bce91b304042154",
+    "url": "/activity-logs/static/js/2.c3547f76.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/activity-logs/static/js/2.c3547f76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20829c8a2b3d664a149f",
+    "url": "/activity-logs/static/js/main.4ac5ad7d.chunk.js"
+  },
+  {
+    "revision": "9ae2fc00196157555295",
+    "url": "/activity-logs/static/js/runtime-main.9b3108a1.js"
+  }
+]);
